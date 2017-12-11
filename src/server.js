2433ad7a0ca742c31cc5be069885057d44b6903d
Sputnik <inquiry@dermah.com>
2017-12-12 00:52:03 +1100

    [WIP] attempt to parse lambda body into file stream

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 import { graphqlLambda, graphiqlLambda } from 'apollo-server-lambda';
+import { processRequest } from 'apollo-upload-server'
+import stream from 'stream';
+
+import multipart from 'aws-lambda-multipart-parser';
 
 import * as Schema from './schema';
 
@@ -26,20 +30,85 @@ const contextFunction =
   };
 
 
-exports.graphqlHandler = graphqlLambda(async (request) => {
-  if (!schema) {
-    schema = schemaFunction(process.env)
-  }
-  const context = await contextFunction(request.headers, process.env);
-  const rootValue = await rootFunction(request.headers, process.env);
+exports.graphqlHandler = (event, context, callback) => {
+  const handler = graphqlLambda(async (request) => {
+    console.log('hello it me')
+    if (!schema) {
+      schema = schemaFunction(process.env)
+    }
+    const context = await contextFunction(request.headers, process.env);
+    const rootValue = await rootFunction(request.headers, process.env);
 
-  return {
-    schema: await schema,
-    rootValue,
-    context,
-    tracing: true,
-  };
-});
+    return {
+      schema: await schema,
+      rootValue,
+      context,
+      tracing: true,
+    };
+  });
+
+
+  if (event.headers["Content-Type"].contains('multipart/form-data; ')) {
+    console.log('OH NO IT A BIGGUN');
+    // console.log(event)
+    // console.log(context);
+
+    // var bodyBuffer = new Buffer(event['body'].toString(),'base64');
+    // var boundary = getBoundary(event.headers["Content-Type"]);
+
+    // console.log(boundary)
+    // console.log(bodyBuffer)
+    // console.log('uh')
+    var parts = multipart.parse(event)
+    // console.log('hey')
+    console.log(parts)
+    // console.log(parts[""])
+
+    // Initiate the source
+    const bufferStream = new stream.PassThrough();
+    // Write your buffer
+    bufferStream.end(parts[""].content);
+    // Pipe it to something else  (i.e. stdout)
+    console.log({
+      stream: bufferStream, //(FileStream)
+      filename: parts[""].filename,
+      mimetype:  parts[""].contentType,
+      encoding:  parts[""].type
+    })
+
+    event.body = {
+      // ...event.body,
+      ...JSON.parse(parts.operations),
+      variables: {
+        // ...event.body.variables,
+        // ...JSON.parse(parts.operations).variables
+        file: {
+          stream: bufferStream, //(FileStream)
+          filename: parts[""].filename,
+          mimetype:  parts[""].contentType,
+          encoding:  parts[""].type
+        }
+      },
+    }
+    // { query: 'mutation ($file: Upload!) {\n  singleUpload(file: $file)\n}\n',
+    // variables: { file: Promise { [Object] } } }
+
+    for(var i=0;i<parts.length;i++){
+        var part = parts[i];
+        // console.log(part);
+        // will be:
+        // { filename: 'A.txt', type: 'text/plain',
+        //		data: <Buffer 41 41 41 41 42 42 42 42> }
+    }
+
+    // const derp = processRequest(event)
+    // console.log(derp)
+    // derp.then(thing => console.log(thing)).catch(err => console.error(error))
+  }
+  console.log(event);
+  return handler(event, context, callback);
+};
+
 
 exports.graphiqlHandler = graphiqlLambda({
   endpointURL: '/graphql',
