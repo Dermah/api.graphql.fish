9599cc09c30284569bc8e41bbc7a0ad2d68ce3f1
Sputnik <inquiry@dermah.com>
2017-12-11 22:27:47 +1100

    Add apollo-upload-server to server handler

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import serverless from 'serverless-http';
+import { apolloUploadExpress } from 'apollo-upload-server'
 
 import * as Schema from './schema';
 
@@ -34,20 +35,25 @@ const contextFunction =
 
 server.use(cors())
 
-server.use('/graphql', bodyParser.json(), graphqlExpress(async (request) => {
-  if (!schema) {
-    schema = schemaFunction(process.env)
-  }
-  const context = await contextFunction(request.headers, process.env);
-  const rootValue = await rootFunction(request.headers, process.env);
+server.use(
+  '/graphql',
+  bodyParser.json(),
+  apolloUploadExpress(/* Options */),
+  graphqlExpress(async (request) => {
+    if (!schema) {
+      schema = schemaFunction(process.env)
+    }
+    const context = await contextFunction(request.headers, process.env);
+    const rootValue = await rootFunction(request.headers, process.env);
 
-  return {
-    schema: await schema,
-    rootValue,
-    context,
-    tracing: true,
-  };
-}));
+    return {
+      schema: await schema,
+      rootValue,
+      context,
+      tracing: true,
+    };
+  })
+);
 
 server.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
