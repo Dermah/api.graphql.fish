3571104a3a09f71ba3cc3591f8c0497985a4f405
Sputnik <inquiry@dermah.com>
2017-12-11 21:42:24 +1100

    Use cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+
 
 import * as Schema from './schema';
 
@@ -30,6 +32,8 @@ const contextFunction =
       secrets
     );
   };
+
+server.use(cors())
 
 server.use('/graphql', bodyParser.json(), graphqlExpress(async (request) => {
   if (!schema) {
