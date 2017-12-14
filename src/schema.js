17aab0ac7a90bf01ecec58244522184e816edc2c
Sputnik <inquiry@dermah.com>
2017-12-14 12:07:29 +1100

    Commit more fish to the cause

--- src/schema.js
+++ src/schema.js
@@ -9,7 +9,8 @@ import { makeExecutableSchema } from 'graphql-tools';
 // Construct a schema, using GraphQL schema language
 const typeDefs = `
   type Query {
-		allFish: [Fish]
+    allFish: [Fish]
+    fish (index: Int): Fish
   }
 
 	type Fish {
@@ -23,12 +24,28 @@ const typeDefs = `
 const resolvers = {
   Query: {
     allFish: (root, args, context) => {
-      return [{
-        name: "Goldfish",
-        picture: "https://upload.wikimedia.org/wikipedia/commons/e/e9/Goldfish3.jpg",
-        description: "Golden, small, forgetful"
-      }];
+      return fish;
     },
+    fish: (root, args) => {
+      const {index} = args
+      console.log(Number.isInteger(index))
+      if (Number.isInteger(index)) {
+        const fisho = fish[index];
+        if (fisho) {
+          return fisho
+        } else {
+          return {
+            name: "undefined Fish",
+            picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+            description: "unable to load at this time. Please check your fishing line and try again"
+          }
+        }
+      } else {
+        return fish[
+          Math.floor(Math.random() * fish.length)
+        ];
+      }
+    }
   },
 };
 
@@ -60,3 +77,29 @@ export function context(headers, secrets) {
 //     secrets,
 //   };
 // };
+
+const fish = [{
+  name: "Goldfish",
+  picture: "https://upload.wikimedia.org/wikipedia/commons/e/e9/Goldfish3.jpg",
+  description: "golden, small, forgetful"
+}, {
+  name: "Marlin",
+  picture: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Atlantic_blue_marlin.jpg",
+  description: "hoping you're having a pleasent evening! Thank you! Ok, bye!"
+}, {
+  name: "Snapper",
+  picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Red_Snapper.jpg/220px-Red_Snapper.jpg",
+  description: "the Uber for fish"
+}, {
+  name: "Fangtooth Fish",
+  picture: "https://upload.wikimedia.org/wikipedia/commons/a/a4/Anoplogaster_cornuta_2.jpg",
+  description: "literally the worst. Ugh."
+}, {
+  name: "Seahorses",
+  picture: "https://upload.wikimedia.org/wikipedia/commons/9/9c/Seahorse_Skeleton_Macro_8_-_edit.jpg",
+  description: "not even a fish..."
+}, {
+  name: "Fishburners",
+  picture: "https://fishburners.org/wp-content/themes/wp-fishburners-3.1/img/stamp-icon-circle.png",
+  description: "a community of like minded fish looking to grow"
+}];
